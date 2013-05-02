63dae9895a6ea90025690a28bdff8789650e448f
Jimmy Visse <jimmy.visse@gmail.com>
2013-05-02 20:13:53 +0200

    Correction d'une erreur de frappe sur une variable

--- calculatrice.cpp
+++ calculatrice.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main()
 {
 	cout << "Entrez une operation (+ - * /)" << endl;
-	
+
 	char c;
 	cin >> c;
-	
+
 	cout << "Entrez le premier terme : ";
 	float a;
 	cin >> a;
-	
+
 	cout << "Entrez le second terme : ";
 	float b;
 	cin >> b;
-	
+
 	if(c == '+')
 	{
 		float resultat = a + b;
@@ -24,12 +24,12 @@ int main()
 	}
 	else if(c=='-')
 	{
-		float resul = a-b;
+		float result = a-b;
 		cout << "a - b = " << result << endl;
 	}
-	
+
 	//TODO : Voir le bug tracker
-	
+
 	return 0;
 }
-	
+
