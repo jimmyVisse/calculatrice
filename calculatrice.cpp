4db0145f4622d00b51d19a055635dce4ad6268e1
Shiroy <wacant@free.fr>
2013-05-02 14:05:12 +0200

    Ajoute un première version de la calculatrice

---
+++ calculatrice.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Entrez une operation (+ - * /)" << endl;
+	
+	char c;
+	cin >> c;
+	
+	cout << "Entrez le premier terme : ";
+	float a;
+	cin >> a;
+	
+	cout << "Entrez le second terme : ";
+	float b;
+	cin >> b;
+	
+	if(c == '+')
+	{
+		float resultat = a + b;
+		cout << "a + b = " << resultat << endl;
+	}
+	
+	//TODO : Voir le bug tracker
+	
+	return 0;
+}
+	
